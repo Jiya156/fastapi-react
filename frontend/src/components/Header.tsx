35550cde0323cd782426d5295bb74860f9db5b88
Jiya156 <modijiya156@2006>
2026-03-10 17:59:55 +0530

    Random change

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       zIndex="1000"
     >
       <Flex align="center" as="nav" mr={5}>
-        <Heading as="h1" size="sm">Todos</Heading>
+        <Heading as="h1" size="sm">Todo List</Heading>
         <Separator />
       </Flex>
     </Flex>
